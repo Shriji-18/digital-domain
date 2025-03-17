017aee8bc53b44d545dfaa76dd604d23dbf25983
Shriji-18 <shriupadhyay605@gmail.com>
2025-03-17 17:25:28 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,12 @@
+// NIGHT MODE TOGGLE FUNCTION
+function toggleMode() {
+    document.body.classList.toggle("night-mode");
+   // document.getElementsByClassName("display-4").style.color = "white";
+    document.getElementById("myHeading").style.color = "white";
+
+}
+
+// MOCKUP CHATBOT FUNCTION
+function openChatbot() {
+    alert("Chatbot is under development! Stay tuned.");
+}
